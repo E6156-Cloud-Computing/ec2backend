9cdf351f2a9bfd3d6835ce8ad5cdae8380f378ae
YUE NIU <ny@YUEs-MacBook-Air-3.local>
2023-12-07 15:52:38 -0500

    update tenant

--- backend.py
+++ backend.py
@@ -1,17 +1,30 @@
 from flask import Flask, request, jsonify
 import sqlite3
+import re
+from datetime import datetime
+
+
+def is_valid_email(email):
+    """ check email address"""
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(email_regex, email) is not None
+
 app = Flask(__name__)
 
-
+#date like YYYY-MM-DD
 def init_db():
     conn = sqlite3.connect('tenant.db')
     conn.execute('''
         CREATE TABLE IF NOT EXISTS tenants (
-            tenant_id INTEGER PRIMARY KEY, 
+            email TEXT PRIMARY KEY, 
             gender TEXT CHECK(gender IN ('Male', 'Female')),
             building_name TEXT, 
             room_name TEXT,      
-            phone_number TEXT
+            phone_number TEXT,
+            rent  INTEGER,
+            lease_start_date TEXT, 
+            lease_end_date TEXT,  
+            identity TEXT CHECK(identity IN('Tenant', 'Manager'))
         )
     ''')
     conn.close()
@@ -33,26 +46,46 @@ def get_all_tenants():
 @app.route('/api/Tenant', methods=['POST'])
 def create_tenant():
     data = request.get_json()
-    tenant_id = data.get('tenant_id')
+    email = data.get('email')
     gender = data.get('gender')
     building_name = data.get('building_name', None)
     room_name = data.get('room_name', None)
     phone_number = data.get('phone_number')
-
+    rent= data.get('rent',None)
+    lease_start_date=data.get('lease_start_date')
+    lease_end_date=data.get('lease_end_date',) 
+    identity=data.get('identity')
     # Validate gender
     if gender not in ['Male', 'Female']:
         return jsonify({'error': 'Invalid gender'}), 400
-
+    # Validate email
+    if not is_valid_email(email):
+        return jsonify({'error': 'Invalid email'}), 400
+    # Validate identity
+    if identity not in ['Tenant', 'Manager']:
+        return jsonify({'error': 'Invalid identity'}), 400
     # Validate phone number
     if not (phone_number.isdigit() and len(phone_number) == 10):
         return jsonify({'error': 'Invalid phone number'}), 400
+    # Convert lease dates from strings to datetime objects
+    try:
+        lease_start = datetime.strptime(lease_start_date, '%Y-%m-%d')
+        lease_end = datetime.strptime(lease_end_date, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({'error': 'Invalid date format'}), 400
+    # Validate lease
+    if lease_end < lease_start:
+        return jsonify({'error': 'Invalid lease date'}), 400
+
     try:
         with sqlite3.connect('tenant.db') as conn:
             cur = conn.cursor()
             cur.execute('''
-                INSERT INTO tenants (tenant_id, gender, building_name, room_name, phone_number)
-                VALUES (?, ?, ?, ?, ?)
-            ''', (tenant_id, gender, building_name, room_name, phone_number))
+                INSERT INTO tenants (email, gender, building_name, room_name, phone_number,rent, 
+                        lease_start_date, lease_end_date, indentity)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (email, gender, building_name, room_name, phone_number,rent, 
+                  lease_start_date, lease_end_date, identity,))
             conn.commit()
             return jsonify({'message': 'Tenant added successfully'}), 201
     except sqlite3.IntegrityError:
@@ -60,14 +93,14 @@ def create_tenant():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/api/Tenant/<int:tenant_id>', methods=['GET'])
-def get_tenant_by_id(tenant_id):
+@app.route('/api/Tenant/<email>', methods=['GET'])
+def get_tenant_by_id(email):
     try:
         with sqlite3.connect('tenant.db') as conn:
             conn.row_factory = sqlite3.Row  # This will enable column access by name
             cursor = conn.cursor()
 
-            cursor.execute("SELECT * FROM tenants WHERE tenant_id = ?", (tenant_id,))
+            cursor.execute("SELECT * FROM tenants WHERE email = ?", (email,))
             row = cursor.fetchone()
 
             if row is None:
@@ -78,31 +111,50 @@ def get_tenant_by_id(tenant_id):
             return jsonify(tenant), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-@app.route('/api/Tenant/<int:tenant_id>', methods=['PUT'])
-def update_tenant(tenant_id):
+    
+
+@app.route('/api/Tenant/<email>', methods=['PUT'])
+def update_tenant(email):
     data = request.get_json()
     gender = data.get('gender')
     building_name = data.get('building_name', None)
     room_name = data.get('room_name', None)
     phone_number = data.get('phone_number')
-
+    rent= data.get('rent',None)
+    lease_start_date=data.get('lease_start_date')
+    lease_end_date=data.get('lease_end_date',) 
+    identity=data.get('identity')
     # Validate gender
     if gender not in ['Male', 'Female']:
         return jsonify({'error': 'Invalid gender'}), 400
-
+    # Validate email
+    if not is_valid_email(email):
+        return jsonify({'error': 'Invalid email'}), 400
+    # Validate identity
+    if identity not in ['Tenant', 'Manager']:
+        return jsonify({'error': 'Invalid identity'}), 400
     # Validate phone number
     if not (phone_number.isdigit() and len(phone_number) == 10):
         return jsonify({'error': 'Invalid phone number'}), 400
-
+    try:
+        lease_start = datetime.strptime(lease_start_date, '%Y-%m-%d')
+        lease_end = datetime.strptime(lease_end_date, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({'error': 'Invalid date format'}), 400
+    # Validate lease
+    if lease_end < lease_start:
+        return jsonify({'error': 'Invalid lease date'}), 400
     # Update data in the database
     try:
         with sqlite3.connect('tenant.db') as conn:
             cur = conn.cursor()
             cur.execute('''
                 UPDATE tenants
-                SET gender = ?, building_name = ?, room_name = ?, phone_number = ?
-                WHERE tenant_id = ?
-            ''', (gender, building_name, room_name, phone_number, tenant_id))
+                SET gender = ?, building_name = ?, room_name = ?, phone_number = ?,
+                rent = ?, lease_start_date= ?, lease_end_date= ?, identity= ?
+                WHERE email= ?
+            ''', (gender, building_name, room_name, phone_number, rent,
+                  lease_start_date, lease_end_date, identity, email,))
             conn.commit()
 
             if cur.rowcount == 0:
@@ -113,13 +165,13 @@ def update_tenant(tenant_id):
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/api/Tenant/<int:tenant_id>', methods=['DELETE'])
-def delete_tenant(tenant_id):
+@app.route('/api/Tenant/<email>', methods=['DELETE'])
+def delete_tenant(email):
     try:
         with sqlite3.connect('tenant.db') as conn:
             cursor = conn.cursor()
 
-            cursor.execute("DELETE FROM tenants WHERE tenant_id = ?", (tenant_id,))
+            cursor.execute("DELETE FROM tenants WHERE email = ?", (email,))
             conn.commit()
 
             if cursor.rowcount == 0:
